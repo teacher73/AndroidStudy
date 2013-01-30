b530604835d2e258ad72937df0fe8262e430f0ea
MinSu Kim <net94@nate.com>
2013-01-30 14:16:22 +0900

    log.d 추가

--- src/kr/saeildamil/androidstudy/fragment/FragmentMain.java
+++ src/kr/saeildamil/androidstudy/fragment/FragmentMain.java
@@ -3,12 +3,50 @@ package kr.saeildamil.androidstudy.fragment;
 import kr.saeildamil.androidstudy.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class FragmentMain extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+		Log.d("Fragment", "FragmentMain - onCreate(Bundle savedInstanceState)");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_main);
 	}
+
+	@Override
+	protected void onDestroy() {
+		Log.d("Fragment", "FragmentMain - onDestroy()");
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		Log.d("Fragment", "FragmentMain - onPause()");
+		super.onPause();
+	}
+
+	@Override
+	protected void onRestart() {
+		Log.d("Fragment", "FragmentMain - onRestart()");
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		Log.d("Fragment", "FragmentMain - onResume()");
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		Log.d("Fragment", "FragmentMain - onStart()");
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		Log.d("Fragment", "FragmentMain - onStop()");
+		super.onStop();
+	}
+	
 }
--- src/kr/saeildamil/androidstudy/fragment/FragmentOne.java
+++ src/kr/saeildamil/androidstudy/fragment/FragmentOne.java
@@ -1,16 +1,81 @@
 package kr.saeildamil.androidstudy.fragment;
 
 import kr.saeildamil.androidstudy.R;
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FragmentOne extends Fragment {
-	@Override
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		Log.d("Fragment", "FragmentOne - onCreateView(LayoutInflater inflater, ViewGroup container,	Bundle savedInstanceState)");
 		return inflater.inflate(R.layout.fragmentone, container, false);
 	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		Log.d("Fragment", "FragmentOne - onActivityCreated(Bundle savedInstanceState)");
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		Log.d("Fragment", "FragmentOne - onAttach(Activity activity)");
+		super.onAttach(activity);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.d("Fragment", "FragmentOne - onCreate(Bundle savedInstanceState)");
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onDestroy() {
+		Log.d("Fragment", "FragmentOne - onDestroy()");
+		super.onDestroy();
+	}
+
+	@Override
+	public void onDestroyView() {
+		Log.d("Fragment", "FragmentOne - onDestroyView()");
+		super.onDestroyView();
+	}
+
+	@Override
+	public void onDetach() {
+		Log.d("Fragment", "FragmentOne - onDetach()");
+		super.onDetach();
+	}
+
+	@Override
+	public void onPause() {
+		Log.d("Fragment", "FragmentOne - onPause()");
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		Log.d("Fragment", "FragmentOne - onResume()");
+		super.onResume();
+	}
+
+	@Override
+	public void onStart() {
+		Log.d("Fragment", "FragmentOne - onStart()");
+		super.onStart();
+	}
+
+	@Override
+	public void onStop() {
+		Log.d("Fragment", "FragmentOne - onStop()");
+		super.onStop();
+	}
+	
+	
 }
